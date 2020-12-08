bd4e13cc716512c0f2846e13e39d02d73307b820
Stas Borovcov <sborovcov@gmail.com>
2020-12-08 15:39:03 +0700

    Initial Commit

---
+++ theLightUI/theLightUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  theLightUIApp.swift
+//  theLightUI
+//
+//  Created by Stas Borovtsov on 08.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct theLightUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
